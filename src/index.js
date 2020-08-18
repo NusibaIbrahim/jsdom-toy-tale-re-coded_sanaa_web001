a739c944c103d797e4c097a2f9866f2a1fde31ba
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-18 10:02:22 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ let addToy = false;
 
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
-let addToy = false
 let divCollect = document.querySelector('#toy-collection')
 
 
